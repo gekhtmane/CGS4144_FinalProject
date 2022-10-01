d8b7933bb5df8a5f702ccf1e0bac9f54bc72b95a
Onoh9 <58475218+Onoh9@users.noreply.github.com>
2022-10-01 17:57:14 -0400

    Add files via upload
    running into formatting issues on the dataset:
    
    Error in h(simpleError(msg, call)) : 
      error in evaluating the argument 'x' in selecting a method for function 'ncol': object 'countData' not found

---
+++ bioinfo_deseq2.R
@@ -0,0 +1,28 @@
+#install.packages("htmltools")
+#library(htmltools)
+#source("https://bioconductor.org/biocLite.R")
+#biocLite("DESeq2")
+setwd("C:/Users/chidi")
+
+BioInfoFile1_2 <- read.csv('C:/Users/chidi/Downloads/BioInfoFile1.3.csv', header=TRUE)
+x <- BioInfoFile1_2[,-(1:9)]
+
+y <- log2(x[,-1]+1) #skip first col which has hugo gene names
+z <- apply(X = y, MARGIN = 1, FUN = median)
+plot(density(z))
+
+library("DESeq2")
+library(ggplot2)
+
+countData <- read.csv('C:/Users/chidi/Downloads/BioInfoFile1.3.csv', header = TRUE, sep = ",")
+head(countData)
+metaData <- read.csv('C:/Users/chidi/Downloads/SeriesMatrixPatIDSampType.csv', header = TRUE, sep = ",")
+metaData
+metaData
+dds <- DESeqDataSetFromMatrix(countData=x, 
+                              colData=metaData, 
+                              design=~dex, tidy = TRUE)
+dds
+dds <- DESeq(dds)
+#res <- results(dds)
+#head(results(dds, tidy=TRUE))
