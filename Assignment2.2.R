0ade70d8d7554f4823791cef11c8cdc1de77c34b
gekhtmane <58517810+gekhtmane@users.noreply.github.com>
2022-10-01 17:17:03 -0400

    Update Assignment2.2.R

--- Assignment2.2.R
+++ Assignment2.2.R
@@ -12,8 +12,17 @@ head(x)
 
 #metadata abt tissue type (primary tumor vs ascites)
 colData <- read_excel('SeriesMatrixPatIDSampType.xlsx')
+#must transpose colData to make its rows match x's columns
 colData <- t(colData)
+#edited colData to skip first row; technically dont need to do this but i want to save it in a new var
+#newColData <- colData[-1,-1]
 
 #col names in countsData (x) must match row names in colData (metadata with tissue type)
-#skip first col in x, skip first row in colData (must transpose)
-all(colnames(x[,-1]) %in% rownames(colData[-1,]))
+#skip first col in x, skip first row in colData (via newColData)
+#all(colnames(x[,-1]) %in% rownames(newColData))
+all(colnames(x[,-1] %in% rownames(colData[-1,-1])))
+
+#same order?
+x[ order(as.numeric(row.names(x))), ]
+print(x)
+all (colnames(x[,-1]) == rownames(newColData))
