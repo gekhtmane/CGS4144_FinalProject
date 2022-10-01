67936a596c05341a2b7880eb4ef0b350e494bc1e
EspKid1109 <68599544+EspKid1109@users.noreply.github.com>
2022-10-01 14:12:31 -0400

    Added new files for part 2

--- NormalizeDataAndDensityPlot.R
+++ NormalizeDataAndDensityPlot.R
@@ -6,5 +6,20 @@ x <- BioInfoFile1[,-(1:9)]
 y <- log2(x+1)
 z <- apply(X = y, MARGIN = 1, FUN = median)
 plot(density(z))
+
+# DESeq2
+library("DESeq2")
+ColFilter <- read.csv("C:/Users/surfc/Desktop/BioInformatics/SeriesMatrixPatIDSampType.csv", header=TRUE)
+ColFilter <- t(ColFilter)
+all(rownames(ColFilter) %in% colnames(x))
+x <- x[, rownames(ColFilter)]
+all(rownames(ColFilter) == colnames(x)) 
+#This Literally just formats the data correctly, using V1 causes and error, Unsure of why this is a problem
+dds <- DESeqDataSetFromMatrix(countData = x,
+                              colData = ColFilter,
+                              design = ~ V1)
+dds <- DESeq(dds)
+res <- results(dds)
+plotCounts(dds,gene=which.min(res$padj), intgroup = "") #DOESNT WORK YET!!!!!!
 end()       
  
