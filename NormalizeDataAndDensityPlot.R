8c4bd54861cad189f7d319762b5b3dafb7c0f349
gracetholl <gracetholl@gmail.com>
2022-10-02 15:28:38 -0400

    PCA Plot and Metadata Transformations

--- NormalizeDataAndDensityPlot.R
+++ NormalizeDataAndDensityPlot.R
@@ -1,25 +1,40 @@
-# First make sure BioInfoFile1 is in your
+# Make sure BioInfoFile1.csv is in your
 # data environment before running this script
-BioInfoFile1 <- read.csv("C:/Users/surfc/Desktop/BioInformatics/BioInfoFile1.csv", header=TRUE)
-x <- BioInfoFile1[,-(1:9)]
+library(readr)
+geo_data <- read.csv("BioInfoFile1.csv", header = TRUE)
+
+geo_data <- geo_data[,-(1:9)]
+
 #boxplot(log2(x+1))
-y <- log2(x+1)
-z <- apply(X = y, MARGIN = 1, FUN = median)
-plot(density(z))
+log_data <- log2(geo_data + 1)
+data_medians <- apply(X = log_data, MARGIN = 1, FUN = median)
+plot(density(data_medians))
 
 # DESeq2
-library("DESeq2")
-ColFilter <- read.csv("C:/Users/surfc/Desktop/BioInformatics/SeriesMatrixPatIDSampType.csv", header=TRUE)
-ColFilter <- t(ColFilter)
-all(rownames(ColFilter) %in% colnames(x))
-x <- x[, rownames(ColFilter)]
-all(rownames(ColFilter) == colnames(x)) 
-#This Literally just formats the data correctly, using V1 causes and error, Unsure of why this is a problem
-dds <- DESeqDataSetFromMatrix(countData = x,
-                              colData = ColFilter,
-                              design = ~ V1)
+library(BiocManager)
+library(DESeq2)
+
+col_data <- read.csv("SeriesMatrixPatIDSampType.csv", header=TRUE)
+# transpose the matrix to match it with the counts data for DESeq2
+col_data <- t(col_data)
+
+# make the first row of col_data a header
+col_name <- col_data[1, ]
+col_data <- data.frame(col_data[2:nrow(col_data), ])
+colnames(col_data) <- c(col_name)
+
+all(rownames(col_data) == colnames(geo_data))
+geo_data <- geo_data[, rownames(col_data)]
+all(rownames(col_data) == colnames(geo_data))
+
+#Formats the data into a DESeqDataSet
+dds <- DESeqDataSetFromMatrix(countData = geo_data, colData = col_data,
+                              design = ~ TissueType)
 dds <- DESeq(dds)
-res <- results(dds)
-plotCounts(dds,gene=which.min(res$padj), intgroup = "") #DOESNT WORK YET!!!!!!
-end()       
+
+# transform dataset to create PCA plot
+normalize_dds <- vst(dds)
+plotPCA(normalize_dds, intgroup = c("TissueType"))
+
+end()     
  
