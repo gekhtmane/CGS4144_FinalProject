6381da8eab0d106b1bc56e69a1a4ed539b22a8f8
Onoh9 <58475218+Onoh9@users.noreply.github.com>
2022-10-26 22:43:00 -0400

    Add files via upload

---
+++ Assgnmt3Bioinformatics.R
@@ -0,0 +1,163 @@
+# Get K-means of data
+
+#First we filter 5000 of the most significant genes
+library(readr)
+library(tidyverse)
+library(dplyr)
+gene_data <- data.frame(read.csv("BioInfoFile1.csv", row.names = 1, header = TRUE))
+col_data <- data.frame(read.csv("SeriesMatrixPatIDSampType.csv", header=TRUE))
+geo_data <- gene_data[,-(1:8)]
+
+# transpose the matrix to match it with the counts data for DESeq2
+col_data <- t(col_data)
+# make the first row of col_data a header
+col_name <- col_data[1, ]
+col_data <- data.frame(col_data[2:nrow(col_data), ])
+colnames(col_data) <- c(col_name)
+
+col_data <- col_data %>% filter(grepl('PrimaryTumour|Ascites',TissueType))
+geo_data <- t(geo_data)
+tokeep <- c(rownames(col_data))
+geo_data <- geo_data[rownames(geo_data) %in% tokeep,]
+geo_data <- t(geo_data)
+
+log_data <- log2(geo_data + 1)
+
+#DIFFSEQ
+library(BiocManager)
+library(DESeq2)
+
+
+#check if in order
+all(rownames(col_data) == colnames(geo_data))
+#if false:
+geo_data <- geo_data[, rownames(col_data)]
+all(rownames(col_data) == colnames(geo_data)) #should now return true
+
+col_data$TissueType <- factor(col_data$TissueType)
+#Formats the data into a DESeqDataSet
+dds <- DESeqDataSetFromMatrix(countData = geo_data, colData = col_data,
+                              design =~ TissueType )
+dds <- DESeq(dds)
+normalize_dds <- vst(dds)
+
+# these next lines are from the Alex' Lemonade tutorial
+if (!("DESeq2" %in% installed.packages())) {
+  # Install this package if it isn't installed yet
+  BiocManager::install("DESeq2", update = FALSE)
+}
+if (!("EnhancedVolcano" %in% installed.packages())) {
+  # Install this package if it isn't installed yet
+  BiocManager::install("EnhancedVolcano", update = FALSE)
+}
+if (!("apeglm" %in% installed.packages())) {
+  # Install this package if it isn't installed yet
+  BiocManager::install("apeglm", update = FALSE)
+}
+
+# Attach the DESeq2 library
+library(DESeq2)
+
+# Attach the ggplot2 library for plotting
+library(ggplot2)
+
+# We will need this so we can use the pipe: %>%
+library(magrittr)
+
+#set seed
+set.seed(12345)
+
+#metadata file is labelled col_data
+#actual data is labelled geo_data (expression_df)
+
+deseq_object <- dds
+deseq_results <- results(deseq_object)
+
+#possible replacement for lfcShrink() line in tutorial:
+res_lfc <- lfcShrink(deseq_object, coef=2, type="apeglm")
+head(res_lfc) #prints table
+
+# filtering/cleaning up using TidyVerse
+deseq_df <- res_lfc %>%
+  # make into data.frame
+  as.data.frame() %>%
+  # the gene names are row names -- let's make them a column for easy display
+  tibble::rownames_to_column("Gene") %>%
+  # add a column for significance threshold results
+  dplyr::mutate(threshold = padj < 0.05) %>%
+  # sort by statistic -- the highest values will be genes with
+  # higher expression in RPL10 mutated samples
+  dplyr::arrange(dplyr::desc(log2FoldChange))
+head(deseq_df)
+
+# Now we filter
+
+# Order data
+geo_data <- geo_data[deseq_df$Gene,]
+log_data <- log_data[deseq_df$Gene,]
+
+# For 5000 genes
+TopGenes <- geo_data[1:5000,]
+TopScale <- log_data[1:5000,]
+TopGenes <- t(TopGenes)
+TopScale <- t(TopScale)
+
+
+dd<-dist(TopScale)
+dd
+hclust<-hclust(dd, method='complete')
+plot(hclust, labels = FALSE)
+
+# Graphing time
+library(factoextra)
+set.seed(123)
+km.res <- kmeans(TopScale, 2, nstart = 25)
+fviz_nbclust(TopScale, kmeans, method = "silhouette")
+# Visualize kmeans clustering
+fviz_cluster(km.res,TopGenes, ellipse.type = "norm", geom = c("point"), main = "5000 Genes")
+alluvial_data <- data.frame(km.res$cluster)
+# make list for numbers of genes
+nums <- list(10, 100, 1000, 10000)
+set.seed(123)
+for(x in nums){
+  TopGenes <- geo_data[1:x,]
+  TopScale <- log_data[1:x,]
+  TopGenes <- t(TopGenes)
+  TopScale <- t(TopScale)
+  km.res <- kmeans(TopScale, 2, nstart = 25)
+  
+  alluvial_data$x <- km.res$cluster
+}
+for(x in nums){
+  TopGenes <- geo_data[1:x,]
+  TopScale <- log_data[1:x,]
+  TopGenes <- t(TopGenes)
+  TopScale <- t(TopScale)
+  
+  dd<-dist(TopScale)
+  dd
+  hclust<-hclust(dd, method='complete')
+  plot(hclust, labels = FALSE)
+}
+
+BiocManager::install("ComplexHeatmap")
+library(ComplexHeatmap)
+Heatmap(TopScale, row_km = 5,
+        show_column_names = FALSE, row_title = NULL, show_row_dend = FALSE)
+
+#Alluvial Plot
+library(ggalluvial)
+head(as.data.frame(TopGenes))
+head(as.data.frame(col_data))
+
+TopGenes$GeneID <- row.names(TopGenes)
+TopGenes$Cluster <- km.res$cluster
+
+ggplot(as.data.frame(as.data.frame(TopGenes)),
+       aes( axis1 = GeneID, axis2 = Cluster)) +
+  geom_alluvium(aes(fill = Admit), width = 1/12) +
+  geom_stratum(width = 1/12, fill = "black", color = "grey") +
+  geom_label(stat = "stratum", aes(label = after_stat(stratum))) +
+  scale_fill_brewer(type = "qual", palette = "Set1") +
+  ggtitle("Gene Cluster Membership")
+
--- NormalizeDataAndDensityPlot.R
+++ NormalizeDataAndDensityPlot.R
@@ -1,7 +1,8 @@
 # Make sure BioInfoFile1.csv is in your
 # data environment before running this script
-library(readr)
 
+# Part 1
+library(readr)
 geo_data <- read.csv("BioInfoFile1.csv", header = TRUE)
 
 geo_data <- geo_data[,-(1:9)]
@@ -11,9 +12,13 @@ log_data <- log2(geo_data + 1)
 data_medians <- apply(X = log_data, MARGIN = 1, FUN = median)
 plot(density(data_medians))
 
-# DESeq2
+# Part 2 - DESeq2
 library(BiocManager)
 library(DESeq2)
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("DESeq2")
 
 col_data <- read.csv("SeriesMatrixPatIDSampType.csv", header=TRUE)
 # transpose the matrix to match it with the counts data for DESeq2
@@ -24,65 +29,240 @@ col_name <- col_data[1, ]
 col_data <- data.frame(col_data[2:nrow(col_data), ])
 colnames(col_data) <- c(col_name)
 
+#check if in order
 all(rownames(col_data) == colnames(geo_data))
+#if false:
 geo_data <- geo_data[, rownames(col_data)]
-all(rownames(col_data) == colnames(geo_data))
+all(rownames(col_data) == colnames(geo_data)) #should now return true
 
+col_data$TissueType <- factor(col_data$TissueType)
 #Formats the data into a DESeqDataSet
 dds <- DESeqDataSetFromMatrix(countData = geo_data, colData = col_data,
-                              design = ~ SourceName)
+                              design =~ TissueType )
 dds <- DESeq(dds)
 
 # transform dataset to create PCA plot
 normalize_dds <- vst(dds)
+plotPCA(normalize_dds, intgroup = c("TissueType"))
 
-plotPCA(normalize_dds, intgroup = c("Tissue Type"))
-#takes multiple gene data, much more than 4 dimensions/variables, and helps
-#make a 2d visual of the clustered data. can also reveal which gene is the most
-#responsible for clustering the data and tell us how accurate the 2d graph is
-
-install.packages("M3C")
+#t-sne plot using M3C package
+if (!require("BiocManager", quietly = TRUE)){
+  install.packages("BiocManager")
+}
+BiocManager::install("M3C", force = TRUE)
 library(M3C)
+tsne(geo_data,colvec=c('gold'))
 
-tsne(geo_data,colvec=c('gold')) 
-# we use tsne with t distribution to visualize the 3d representation of the gene
-#distribution in a 2d plane
 
-##Tdata <- read.csv("BioInfoFile1.csv", header = TRUE)  data(geo_data)
-##umap(geo_data,colvec=c('skyblue'))
-##bio.umap <- umap(geo_data)
+# Part 3
+# these next lines are from the Alex' Lemonade tutorial
+if (!("DESeq2" %in% installed.packages())) {
+  # Install this package if it isn't installed yet
+  BiocManager::install("DESeq2", update = FALSE)
+}
+if (!("EnhancedVolcano" %in% installed.packages())) {
+  # Install this package if it isn't installed yet
+  BiocManager::install("EnhancedVolcano", update = FALSE)
+}
+if (!("apeglm" %in% installed.packages())) {
+  # Install this package if it isn't installed yet
+  BiocManager::install("apeglm", update = FALSE)
+}
 
-##bio.umap
+# Attach the DESeq2 library
+library(DESeq2)
 
-##head(bio.umap$layout, 9)
-##plot.bio(bio.umap, bio.labels)
+# Attach the ggplot2 library for plotting
+library(ggplot2)
 
-install.packages("gprofiler2")
+# We will need this so we can use the pipe: %>%
+library(magrittr)
+
+#set seed
+set.seed(12345)
+
+#metadata file is labelled col_data
+#actual data is labelled geo_data (expression_df)
+
+deseq_object <- DESeq(dds)
+deseq_results <- results(deseq_object)
+
+#possible replacement for lfcShrink() line in tutorial:
+res_lfc <- lfcShrink(deseq_object, coef=2, type="apeglm")
+head(res_lfc) #prints table
+
+# filtering/cleaning up using TidyVerse
+deseq_df <- res_lfc %>%
+  # make into data.frame
+  as.data.frame() %>%
+  # the gene names are row names -- let's make them a column for easy display
+  tibble::rownames_to_column("Gene") %>%
+  # add a column for significance threshold results
+  dplyr::mutate(threshold = padj < 0.05) %>%
+  # sort by statistic -- the highest values will be genes with
+  # higher expression in RPL10 mutated samples
+  dplyr::arrange(dplyr::desc(log2FoldChange))
+
+head<-head(deseq_df, 10000)
+head
+dd<-dist(head)
+dd
+hclust<-hclust(dd, method='complete')
+plot(hclust, labels=FALSE, hang=-1)
+
+#save tsv file
+readr::write_tsv(
+  deseq_df,
+  file.path(
+    getwd(),
+    "DiffExp.tsv" # Replace with a relevant output file name
+  )
+)
+
+#volcano plot
+volcano_plot <- EnhancedVolcano::EnhancedVolcano(
+  deseq_df,
+  lab = deseq_df$Gene,
+  x = "log2FoldChange",
+  y = "padj",
+  pCutoff = 0.01 # Loosen the cutoff since we supplied corrected p-values
+)
+volcano_plot
+
+# Part 4
+BiocManager::install("ComplexHeatmap")
+library(ComplexHeatmap)
+
+# get statistically significant list of expressed genes from deseq_df
+sig_deseq <- deseq_df[deseq_df$pvalue < 0.001, ]
+
+# counts matrix
+dds_matrix <- counts(dds[rownames(dds) %in% sig_deseq$Gene])
+# get z score values of matrix
+z_matrix <- t(apply(dds_matrix, 1, scale))
+
+#Heatmap of matrix, side map
+Heatmap(z_matrix, row_km = 5, 
+        col = colorRamp2(c(-2, 0, 2), c("green", "white", "red")),
+        show_column_names = FALSE, row_title = NULL, show_row_dend = FALSE)
+
+Heatmap(col_data$TissueType, name = "sample groupings",
+        top_annotation = HeatmapAnnotation(summary = anno_summary(height = unit(2, "cm"))),
+        width = unit(15, "mm"))
+
+# Part 5 - topGO
+BiocManager::install("topGO")
+library(topGO)
+
+# create named vector with genes and p values
+deseq_vector <- setNames(deseq_df$pvalue, deseq_df$Gene)
+deseq_vector <- deseq_vector[!is.na(deseq_vector)]
+
+# statistically significant vector
+sig_vector <- setNames(sig_deseq$pvalue, sig_deseq$Gene)
+
+# below code is taken from the topGO library documentation
+topDiffGenes <- function(pvalue) {
+  return(pvalue < 0.01)
+}
+gene_sel <- topDiffGenes(deseq_vector)
+
+# set annotation to specific db and gene ID
+allGO2genes <- annFUN.org(
+  whichOnto = "BP",
+  feasibleGenes = NULL,
+  mapping = "org.Hs.eg.db",
+  ID = "ensembl")
+
+GOdata <- new("topGOdata",
+              ontology = "BP",
+              description = "Enrichment Analysis Data using topGO Methodology",
+              allGenes = deseq_vector,
+              annot = annFUN.GO2genes,
+              GO2genes = allGO2genes,
+              geneSel = topDiffGenes)
+
+# now use GOdata to perform enrichment analysis 
+# run Fisher test
+result_fisher <- runTest(GOdata, algorithm = "classic", statistic = "fisher")
+
+# Kolmogorov-Smirnov
+result_KS_elim <- runTest(GOdata, algorithm = "elim", statistic = "ks")
+result_KS_classic <- runTest(GOdata, algorithm = "classic", statistic = "ks")
+
+results_table <- GenTable(GOdata, classicFisher = result_fisher,
+                          classicKS = result_KS_classic,
+                          elimKS = result_KS_elim, orderBy = "elimKS",
+                          ranksOf = "classicFisher", topNodes = 10)
+
+#save csv file
+write.table(results_table, file='topGO.csv', quote=FALSE, sep=',')
+
+
+BiocManager::install("Rgraphviz")
+# compare values - code based on topGO R vignette
+showSigOfNodes(GOdata, score(result_KS_elim), firstSigNodes = 5, useInfo = 'all')
+
+# Part 5 - clustProfiler
+library(clusterProfiler)
+
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("org.Hs.eg.db")
+library(org.Hs.eg.db)
+
+ggo <- groupGO(gene     = deseq_df$Gene,
+               OrgDb    = org.Hs.eg.db,
+               ont      = "CC",
+               level    = 3,
+               readable = TRUE)
+head(ggo)
+
+# create a geneList that is geneName | log2foldchange
+geneList = deseq_df$log2FoldChange
+names(geneList) = as.character(deseq_df$Gene)
+
+geneList = sort(geneList, decreasing = TRUE)
+
+ego3 <- gseGO(geneList     = geneList,
+              OrgDb        = org.Hs.eg.db,
+              ont          = "CC",
+              minGSSize    = 100,
+              maxGSSize    = 60000,
+              pvalueCutoff = 0.05,
+              verbose      = FALSE)
+head(ego3)
+
+#save tsv file
+write.table(ego3, file='ClustProfiler.tsv', quote=FALSE, sep='\t')
+
+
+# gprofiler2 - Disease Ontology
+# Disease Ontology using gprofiler2
+if (!("gprofiler2" %in% installed.packages())) {
+  # Install this package if it isn't installed yet
+  install.packages("gprofiler2", update = FALSE)
+}
+if (!("DOSE" %in% installed.packages())) {
+  # Install this package if it isn't installed yet
+  BiocManager::install("DOSE", update = FALSE)
+}
+
 library(gprofiler2)
-gostres <- gost(query = c("GO:0005198"), 
-                organism = "hsapiens", ordered_query = FALSE, 
-                multi_query = FALSE, significant = TRUE, exclude_iea = FALSE, 
-                measure_underrepresentation = FALSE, evcodes = FALSE, 
-                user_threshold = 0.05, correction_method = "g_SCS", 
-                domain_scope = "annotated", custom_bg = NULL, 
-                numeric_ns = "", sources = NULL, as_short_link = FALSE)
-names(gostres)
-head(gostres$result, 3)
-##multi_gostres2 <- gost(query = list("chromX" = c("GO:0005198"),
-##                                    "chromY" = c("GO:0005829"),
-##                                    "chromZ" = c("GO:0070268")), 
-##                       multi_query = TRUE)
-##names(gostres2)
-##head(gostres2$result, 3)
-gostplot(gostres, capped = TRUE, interactive = TRUE)
-head(gostres$result, 3)
-end()       
-##names(gostres$meta)
-##gostres2 <- gost(query = c("ENSG00000000003", "TSPAN6"), 
-##                 organism = "hsapiens", ordered_query = FALSE, 
-##                 multi_query = FALSE, significant = TRUE, exclude_iea = FALSE, 
-##                 measure_underrepresentation = FALSE, evcodes = TRUE, 
-##                 user_threshold = 0.05, correction_method = "g_SCS", 
-##                 domain_scope = "annotated", custom_bg = NULL, 
-##                 numeric_ns = "", sources = NULL)
-##head(gostres2$result, 3) 
+library(DOSE)
+
+#read the file
+genelist <- read.table(file = 'DiffExp.tsv', sep = '\t', header = TRUE)
+geneList = genelist[,3]
+names(geneList) = as.character(genelist[,1])
+geneList = sort(geneList, decreasing = TRUE)
+head(geneList)
+y <- gseDO(geneList,
+           minGSSize     = 120,
+           pvalueCutoff  = 0.2,
+           pAdjustMethod = "BH",
+           verbose       = FALSE)
+head(y)
+write.table(y, file='gp2DOenrichment.tsv', quote=FALSE, sep='\t')
+end()
